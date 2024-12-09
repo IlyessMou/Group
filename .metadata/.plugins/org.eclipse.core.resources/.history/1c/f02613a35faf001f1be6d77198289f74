b5444cae8224f041b60a9f3603fa22b001177a4b
unknown <ilyessmoulazem1@gmail.com>
2024-12-09 20:33:30 +0100

    quizzapp

--- .metadata/.plugins/org.eclipse.core.resources/.history/1c/f02613a35faf001f1be6d77198289f74
+++
@@ -1,8 +0,0 @@
-package com.example.quizza.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.quizza.models.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
--- quizzapp/src/main/java/com/example/quizzapp/repositories/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.quizzapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.quizzapp.models.Question;
-
-@Repository
-public interface QuestionRepository extends CrudRepository<Question, Long> {}
